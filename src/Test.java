6fb3550d82db0739d02d6aff79693ae67f6f2fc2
manoj <manojs121@rediffmail.com>
2020-06-06 12:13:17 +0530

    06-06-2020

--- src/Test.java
+++ src/Test.java
@@ -4,7 +4,7 @@ public class Test {
     
     public static void main(String args[]){
         int a,b=2;
-        System.out.println(a);
+        //System.out.println(a);
         System.out.println(b);
     }
 }
