4e37cb1ea9469952fab2bc79556f18c509540d32
manoj <manojs121@rediffmail.com>
2020-06-16 12:15:34 +0530

    16-06-2020

--- src/Demo.java
+++ src/Demo.java
@@ -3,6 +3,15 @@ public class Demo {
 
     private int x, y;
     
+    //we are writing a method to compare two objects of Demo class.
+    public boolean equals(Demo obj){
+        if(x==obj.x && y==obj.y){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
     public void show(){
         System.out.println(x+","+y);
     }
@@ -12,6 +21,19 @@ public class Demo {
     }
     
     public static void main(String[] args) {
+        
+       
+        Demo d1=new Demo(10,20);
+        Demo d2=new Demo(10,20);
+        Demo d3=d2;
+        
+        boolean res1=(d1==d2);
+        System.out.println(res1);
+        
+        boolean res2=d1.equals(d2);
+        System.out.println(res2);
+        
+        /*
         Demo d1;                    //declaring a reference of type Demo
         new Demo(10,20);            //object created but address is not stored in any ref (cant be accessed)
         Demo d2=new Demo(30,40);
@@ -26,6 +48,6 @@ public class Demo {
         System.out.println(d1.x+","+d1.y);
         System.out.println(d2.x+","+d2.y);
         System.out.println(d3.x+","+d3.y);
-       
+       */
     }
 }
